e9752eb1bcfdc2d1a05e8faa8ca8d41379b53976
Gabriel W. Bartmanovicz <obielwb@gmail.com>
2025-09-19 12:05:00 -0300

    feat: update code

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -12,12 +12,16 @@ def draw_plot():
     # Create first line of best fit (using all data)
     slope, intercept, _, _, _ = linregress(df['Year'], df['CSIRO Adjusted Sea Level'])
     years_extended = pd.Series(range(1880, 2051))
-    plt.plot(years_extended, slope * years_extended + intercept, 'r', label='Fit 1880-2014')
+    line1_y = slope * years_extended + intercept
+    plt.plot(years_extended, line1_y, 'r', label='Fit 1880-2014')
     
     # Create second line of best fit (using data from 2000 onward)
     recent_data = df[df['Year'] >= 2000]
     slope_recent, intercept_recent, _, _, _ = linregress(recent_data['Year'], recent_data['CSIRO Adjusted Sea Level'])
-    plt.plot(years_extended, slope_recent * years_extended + intercept_recent, 'g', label='Fit 2000-2014')
+    # Only plot from 2000 to 2050 for the second line
+    years_recent_extended = pd.Series(range(2000, 2051))
+    line2_y = slope_recent * years_recent_extended + intercept_recent
+    plt.plot(years_recent_extended, line2_y, 'g', label='Fit 2000-2014')
     
     # Add labels and title
     plt.xlabel('Year')
